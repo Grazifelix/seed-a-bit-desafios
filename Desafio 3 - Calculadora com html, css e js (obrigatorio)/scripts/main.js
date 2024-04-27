4f7d3bf979c21c5180a06d38cd424f7c001ac736
GrazielaFelix <grazielafelix.dev@gmail.com>
2024-04-27 16:11:24 -0300

    :sparkles: feat: selecionando elementos html

---
+++ Desafio 3 - Calculadora com html, css e js (obrigatorio)/scripts/main.js
@@ -0,0 +1,31 @@
+// let num = document.getElementsByClassName("number")
+// let button = document.getElementsByTagName("button")
+
+const button_zero = document.querySelector("#button-0")
+const button_one = document.querySelector("#button-1")
+const button_two = document.querySelector("#button-2")
+const button_three = document.querySelector("#button-3")
+const button_four = document.querySelector("#button-4")
+const button_five = document.querySelector("#button-5")
+const button_six = document.querySelector("#button-6")
+const button_seven = document.querySelector("#button-7")
+const button_eight = document.querySelector("#button-8")
+const button_nine = document.querySelector("#button-9")
+
+/*Operadores aritméricos*/
+const button_sum = document.querySelector("#button-sum")
+const button_divide = document.querySelector("#button-division")
+const button_multi = document.querySelector("#num-mult")
+const button_sub = document.querySelector("#button-sub")
+const button_iqual = document.querySelector("#button-igual")
+const button_dot = document.querySelector("#button-dot")
+
+
+const button_CE = document.querySelector("#button-CE")
+const button_C = document.querySelector("#button-C")
+
+// Numeros na tela
+const num_principal = document.querySelector('#num-principal')
+const num_secundario = document.querySelector('#num-secundario')
+
+
